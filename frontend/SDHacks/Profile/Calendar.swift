deb0598d6fa0e3f7d1fc4c5d7ca274b0f0bce243
HarinHaoWu <master.harin@gmail.com>
2021-02-21 01:11:20 -0800

    Profile Done

--- frontend/SDHacks/Profile/Calendar.swift
+++ frontend/SDHacks/Profile/Calendar.swift
@@ -1,5 +1,5 @@
 //
-//  Profile.swift
+//  Calendar.swift
 //  SDHacks
 //
 //  Created by Harin Wu on 2021-02-21.
@@ -7,13 +7,27 @@
 
 import SwiftUI
 
-struct Profile: View {
+struct Calendar: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack {
                 HStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            HStack {
+                            Image("Back") // set image here
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.white)
+                            }
+                    }.padding(.leading, 25).padding(.top, 30)
+                    Spacer()
+                }.padding(.bottom, 10)
+                HStack {
                     VStack {
-                        Text("Harin's").font(.system(size: 16)).foregroundColor(Color(hex: "#C4C4C4")).padding(.leading, 25).padding(.top, 5).frame(minWidth: 0, maxWidth: 300, alignment: .leading)
+                        Text("Harin's Monthly").font(.system(size: 16)).foregroundColor(Color(hex: "#C4C4C4")).padding(.leading, 25).padding(.top, 5).frame(minWidth: 0, maxWidth: 300, alignment: .leading)
                         Text("Greenhouse").font(.system(size: 20)).padding(.leading, 25).frame(minWidth: 0, maxWidth: 300, alignment: .leading)
                     }
                     Image("harin")
@@ -22,36 +36,21 @@ struct Profile: View {
                         .frame(width: 50, height: 50)
                         .cornerRadius(50)
                     
-                }.padding(.top, 50)
+                }.padding(.top, 20)
                 HStack {
-                    Button(action:{}) {
-                        Text("Saved Plants")
-                            .font(.system(size: 20))
-                            .foregroundColor(.black)
-                    }.padding(.leading, 25)
-                    Spacer()
-                    NavigationLink(destination: Plots()) {
-                        Text("See all")
-                            .font(.system(size: 18))
-                            .foregroundColor(.black)
-                            .padding(.trailing, 25)
-                    }
+                    Image("BigCal")
+                                   .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                   .frame(width: 329, height: 329)
+                                   .edgesIgnoringSafeArea(.all)
                 }.padding(.top, 30)
-                PlantScroll()
-                    .offset(y: -10)
                 HStack {
                     Button(action:{}) {
-                        Text("History")
+                        Text("8 Contributions")
                             .font(.system(size: 20))
                             .foregroundColor(.black)
                     }.padding(.leading, 25)
                     Spacer()
-                    NavigationLink(destination: Calendar()) {
-                        Image("Calendar") // set image here
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(.white)
-                            .padding(.trailing, 25)
-                    }
                 }.padding(.top, 30)
                 VStack {
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
@@ -60,7 +59,7 @@ struct Profile: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(minWidth: 0, maxWidth: 50, alignment: .leading)
                             .padding(.all, 5)
-                        Text("38 times")
+                        Text("2 times")
                             .font(.system(size: 16))
                             .foregroundColor(Color(hex: "#848484"))
                             .fontWeight(.medium)
@@ -76,7 +75,7 @@ struct Profile: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(minWidth: 0, maxWidth: 50, alignment: .leading)
                             .padding(.all, 5)
-                        Text("150 times")
+                        Text("3 times")
                             .font(.system(size: 16))
                             .foregroundColor(Color(hex: "#848484"))
                             .fontWeight(.medium)
@@ -92,7 +91,7 @@ struct Profile: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(minWidth: 0, maxWidth: 50, alignment: .leading)
                             .padding(.all, 5)
-                        Text("53 times")
+                        Text("1 time")
                             .font(.system(size: 16))
                             .foregroundColor(Color(hex: "#848484"))
                             .fontWeight(.medium)
@@ -108,7 +107,7 @@ struct Profile: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(minWidth: 0, maxWidth: 50, alignment: .leading)
                             .padding(.all, 5)
-                        Text("20 times")
+                        Text("2 times")
                             .font(.system(size: 16))
                             .foregroundColor(Color(hex: "#848484"))
                             .fontWeight(.medium)
@@ -120,15 +119,13 @@ struct Profile: View {
                     )
                 }.padding(.top, 30)
                 Spacer()
-            }.navigationBarTitle("")
-            .navigationBarBackButtonHidden(true)
-            .navigationBarHidden(true)
+            }
         }
     }
 }
 
-struct Profile_Previews: PreviewProvider {
+struct Calendar_Previews: PreviewProvider {
     static var previews: some View {
-        Profile()
+        Calendar()
     }
 }
