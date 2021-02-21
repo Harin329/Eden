b0ee25183748e1b1ccf85cca4641515eebcf5140
victor-uemura <uemura.v@gmail.com>
2021-02-21 01:13:42 -0800

    onboarding slides

--- frontend/SDHacks/Onboarding/Onboarding.swift
+++ frontend/SDHacks/Onboarding/Onboarding.swift
@@ -20,13 +20,12 @@ struct Onboarding: View {
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                 .offset(y: -87)
                 
-                NavigationLink(destination: VStack{}) {
+                NavigationLink(destination: Onboarding1(login: $login, map: $map)) {
                     Image("ob0")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                        .offset(x: 100, y: 100)
-                }
+                        .frame(width: 50, height: 50)
+                }.offset(x: 130, y: 280)
             }
             .navigationBarTitle("")
             .navigationBarBackButtonHidden(true)
@@ -35,4 +34,56 @@ struct Onboarding: View {
     }
 }
 
+struct Onboarding1: View {
+    @Binding var login: Bool
+    @Binding var map: Bool
+
+    var body: some View {
+            ZStack {
+               Image("onboarding1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .offset(y: 0)
+                
+                NavigationLink(destination: Onboarding2(login: $login, map: $map)) {
+                    Image("ob1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 72, height: 72)
+                }.offset(x: 130, y: 275)
+            }
+            .navigationBarTitle("")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+    }
+}
+
+struct Onboarding2: View {
+    @Binding var login: Bool
+    @Binding var map: Bool
+
+    var body: some View {
+            ZStack {
+               Image("onboarding2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .offset(y: 0)
+                
+                Button(action: {
+                    login = false
+                    map = true
+                }) {
+                    Image("ob2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 72, height: 72)
+                }.offset(x: 125, y: 275)
+            }
+            .navigationBarTitle("")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+    }
+}
 
